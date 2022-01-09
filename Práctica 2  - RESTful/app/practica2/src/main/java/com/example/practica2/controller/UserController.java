3c9e93e995e32ce82441cf36849e68486f3ff031
diego <diegohm1598@gmail.com>
2022-01-09 01:35:50 +0100

    Arreglando errores

--- Práctica 2  - RESTful/app/practica2/src/main/java/com/example/practica2/controller/UserController.java
+++ Práctica 2  - RESTful/app/practica2/src/main/java/com/example/practica2/controller/UserController.java
@@ -3,32 +3,13 @@ package com.example.practica2.controller;
 import com.example.practica2.model.Product;
 import com.example.practica2.model.User;
 import com.example.practica2.repository.UserRepository;
-import com.example.practica2.security.AuthResponse;
-import com.example.practica2.security.JwtTokenUtil;
-import com.example.practica2.security.UserManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-
-class EmailPassword {
-    public String email;
-    public String password;
-}
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -36,12 +17,6 @@ class EmailPassword {
 class UserController {
 
     private UserRepository _userRepository;
-    @Autowired
-    AuthenticationManager authenticationManager;
-    @Autowired
-    JwtTokenUtil jwtTokenUtil;
-    @Autowired
-    PasswordEncoder encode;
 
     public UserController(UserRepository userRepository) {
         _userRepository = userRepository;
@@ -59,11 +34,17 @@ class UserController {
         return user;
     }
 
-
     @GetMapping("/name/{name}")
     public User getUserByName(@PathVariable String name) {
         User user = _userRepository.findUserByName(name)
         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User " + name + " not exists"));
+        return user;
+    }
+
+    @GetMapping("/email/{email}")
+    public User getUserByEmail(@PathVariable String email) {
+        User user = _userRepository.findUserByEmail(email)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User " + email + " not exists"));
         return user;
     }
 
@@ -73,7 +54,7 @@ class UserController {
         Boolean valueAdmin = false;
         if(admin != null)
             valueAdmin = admin;
-        _userRepository.save(new User(name, email, encode.encode(password), valueAdmin));
+        _userRepository.save(new User(name, email, password, valueAdmin));
     }
 
     @GetMapping("/products")
@@ -81,44 +62,6 @@ class UserController {
         User user = _userRepository.findById((Integer) request.getSession().getAttribute("id"))
         .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not exists"));
         return user.getProducts();
-    }
-
-    @PostMapping(value = "login")
-    public ResponseEntity<Object> login(@RequestParam(value = "email")String email, @RequestParam(value = "password")String password) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String token = jwtTokenUtil.generateJwtToken(authentication);
-        UserManager userAuth = (UserManager) authentication.getPrincipal();
-        List<String> roles = userAuth.getAuthorities().stream()
-        .map(auth -> auth.getAuthority())
-        .collect(Collectors.toList());
-
-        AuthResponse authResponse = new AuthResponse();
-        authResponse.setToken(token);
-        authResponse.setRoles(roles);
-        return ResponseEntity.ok(authResponse);
-
-    }
-
-    @PostMapping("/signup")
-    public ResponseEntity<?> userSignup(@RequestParam(value = "name")String name,
-    @RequestParam(value = "email")String email, @RequestParam(value = "password")String password) {
-        User user = new User();
-        user.setEmail(email);
-        user.setPassword(encode.encode(password));
-        user.setName(name);
-        user.setAdmin(false);
-        _userRepository.save(user);
-        return ResponseEntity.ok("User signed up successfully");
-    }
-
-    @GetMapping("logout")
-    public void logout(HttpServletRequest session) {
-        HttpSession httpSession = session.getSession(false);
-
-        if (httpSession != null)
-            httpSession.invalidate();
     }
 
     @PutMapping("/{id}")
--- Práctica 2  - RESTful/app/practica2/src/main/java/com/example/practica2/security/Auth.java
+++ Práctica 2  - RESTful/app/practica2/src/main/java/com/example/practica2/security/Auth.java
@@ -1,25 +1,11 @@
 package com.example.practica2.security;
 
-import com.example.practica2.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
 @Configuration
@@ -27,73 +13,16 @@ import org.springframework.security.web.header.writers.StaticHeadersWriter;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class Auth extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    JwtAuthenticationEntryPoint authenticationEntryPoint;
-
-    @Bean
-    public PasswordEncoder encode() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(encode());
-        /*auth.userDetailsService(userDetailsService).passwordEncoder(getEncoder());
-        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");*/
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Bean
-    public JwtTokenFilter jwtTokenFilter(){
-        return new JwtTokenFilter();
-    }
-    /*
-
-            httpSecurity.httpBasic().and().authorizeRequests()
-                .antMatchers("/").permitAll()
-        .antMatchers("/products").hasAnyRole("ADMIN").and().formLogin();
-     */
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors().and().csrf().disable()
-        .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint).and()
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        /*.and().authorizeRequests().antMatchers("/users/**").permitAll()
-        .anyRequest().permitAll()*/
-                .and().authorizeRequests().antMatchers("/**").permitAll()
-        /*.antMatchers(HttpMethod.GET, "/**").permitAll()
-        .antMatchers(HttpMethod.POST, "/products/**").hasRole("ADMIN")
-        .antMatchers(HttpMethod.PUT, "/products/**").permitAll()
-        .antMatchers(HttpMethod.DELETE, "/products/**").permitAll()
-        .antMatchers(HttpMethod.POST, "/users/**").permitAll()
-        .antMatchers(HttpMethod.PUT, "/users/**").permitAll()
-        .antMatchers(HttpMethod.DELETE, "/users/**").permitAll().and()*/
-        ; //.anyRequest().authenticated().and().formLogin()
-
+        //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+        .authorizeRequests().antMatchers("/**").permitAll()
+        //.authenticated().and().formLogin()
+        ;
 
         httpSecurity.headers()
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Request-Methods", "*"));
-
-        httpSecurity.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-        /*
-                httpSecurity.httpBasic().and().authorizeRequests()
-        .antMatchers(HttpMethod.GET, "/**").permitAll()
-        .antMatchers(HttpMethod.POST, "/products/**").permitAll()
-        .antMatchers(HttpMethod.PUT, "/products/**").permitAll()
-        .antMatchers(HttpMethod.DELETE, "/products/**").permitAll()
-        .antMatchers(HttpMethod.POST, "/users/**").permitAll()
-        .antMatchers(HttpMethod.PUT, "/users/**").permitAll()
-        .antMatchers(HttpMethod.DELETE, "/users/**").permitAll()//.hasRole("ADMIN")
-        .and().csrf().disable()
-         */
+        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"))
+        .addHeaderWriter(new StaticHeadersWriter("Access-Control-Request-Methods", "*"));
     }
 }
